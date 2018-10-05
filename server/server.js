deb56ddc8144dc1df18214a1798259537c7656a8
Pierre Evrard <evrardp12@gmail.com>
2018-10-05 10:21:42 +0200

    Fix typo

--- server/server.js
+++ server/server.js
@@ -1,34 +1,34 @@
 
-var express = require("express");
+var express = require('express');
 var app = express();
 
 var server = require('http').Server(app);
 var socketio = require('socket.io').listen(server);
-//socketio.set('transports', ['websocket','flashsocket','htmlfile','xhr-polling','jsonp-polling']);
+// socketio.set('transports', ['websocket','flashsocket','htmlfile','xhr-polling','jsonp-polling']);
 
 
 var redis = null;
-/*if (!process.env.SHORTFUSE_LIGHT)
-     redis = require("redis").createClient();*/
+// if (!process.env.SHORTFUSE_LIGHT)
+//      redis = require("redis").createClient();
 
-/////////////
+// ///////////
 
 
 global.counters = {};
-//var monitor = require("./monitor.js").start({io: socketio, redis: redis});
+// var monitor = require("./monitor.js").start({io: socketio, redis: redis});
 
-var Server = require("./game/server");
+var Server = require('./game/server');
 
-new Server({io: socketio, redis: null /*redis*/});
+new Server({ io: socketio, redis: null /* redis*/ });
 
 
 app.use(express.bodyParser());
 
 // game
-app.use(express.static(__dirname + "/../web/game/"));
+app.use(express.static(__dirname + '/../web/game/'));
 
 // monitor
-//app.use("/monitor/", express.static(__dirname + "/../web/monitor/"));
+// app.use("/monitor/", express.static(__dirname + "/../web/monitor/"));
 
 server.listen(8181);
 
